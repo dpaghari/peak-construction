475aebccf728215f8d7093df56a59d880a29ae48
Daniel Pagharion <danp@digit.co>
2018-12-20 13:13:29 -0800

    initial setup, homepage

--- client/client.js
+++ client/client.js
@@ -1,48 +1,5 @@
 $(document).ready(function (){
-  let $navIcon = $('.nav-icon');
-  $navIcon.on('click', function () {
-    $(this).toggleClass('open');
-    $('.menu-container').toggleClass('active');
+  $('.c-splash').on('click', function () {
+    $(this).fadeOut('slow');
   });
-
-  let $splash = document.querySelector('.c-hero');
-  let heroWaypoint = new Waypoint({
-    element: $splash,
-    handler: function(direction) {
-      if(direction === 'down')
-      $splash.classList.add('wp-fadeup');
-    }
-  });
-
-
-  let $projects = Array.from(document.querySelectorAll('.c-projects__project'));
-
-  $projects.map(($el) => {
-    let projectWaypoint = new Waypoint({
-      element: $el,
-      handler: function(direction) {
-        if(direction === 'down')
-        $el.classList.add('wp-fadein');
-      },
-      offset: '35%'
-    });
-  });
-
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-	    var target = this.hash;
-	    var $target = $(target);
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing');
-  });
-
-  // $(document).on('click', '.nerd-note-btn', function(e) {
-  //   e.preventDefault();
-  //   let $nerdNote = $(this).siblings('.nerd-notes')[0];
-  //   console.log($nerdNote);
-  //   // $($nerdNote).toggleClass('active');
-  //   $($nerdNote).slideToggle('fast');
-  //
-  // });
 });

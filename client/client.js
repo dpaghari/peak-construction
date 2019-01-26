1f3a45cec6d51939bc2509bfeba459318ac7c9ef
Daniel Pagharion <danp@digit.co>
2019-01-26 10:23:05 -0800

    hero carousel

--- client/client.js
+++ client/client.js
@@ -2,4 +2,26 @@ $(document).ready(function (){
   $('.c-splash').on('click', function () {
     $(this).fadeOut('slow');
   });
+
+  $('.c-hero .c-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    adaptiveHeight: true,
+  });
+  $('.c-capabilities .c-carousel').slick({
+    prevArrow: $('.c-capabilities__btn--back'),
+    nextArrow: $('.c-capabilities__btn--next'),
+    slidesToShow: 1,
+    infinite: true,
+  });
+  // $('.c-capabilities .c-carousel').slick({
+  //   prevArrow: $('.c-capabilities__btn--back'),
+  //   nextArrow: $('.c-capabilities__btn--next'),
+  //   slidesToShow: 1,
+  //   infinite: true,
+  // });
+
 });
